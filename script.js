9153bc3ed4bcfb38a1634ea0f9ef4873ed1f7866
chenpetro <chenpetro@gmail.com>
2024-09-26 22:24:46 +0200

    commit

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 //     console.log(`this is my ${i} excercise`)
 //   }
 
-let i = 0;
-do {
-  i++;
-  console.log(`this is my ${i} excercise`);
-} while (i < 100);
+// let i = 0;
+// do {
+//   i++;
+//   console.log(`this is my ${i} excercise`);
+// } while (i < 100);
 
 // let password = '';
 // do {
@@ -16,3 +16,40 @@ do {
 // } while (password.length < 5);
 
 // console.log('Entered password: ', password);
+
+
+// for (let index = 0; index < 50; index++) {
+//     console.log(`this is my ${index} excercise`)
+    
+// }
+
+// let card = document.querySelector('.card')
+
+// for (let index = 0; index <= 50; index++) {
+//     card.innerHTML+=`
+//     <div class="card-item">
+//         <img src="./macbook.webp" alt="">
+//         <h2 class="title">Cards</h2>
+//         <p class="desc">Lorem, ipsum dolor.</p>
+//         <p>Це моє ${index} віджимання</p>
+//         <button class="btn">Buy</button>
+//     </div>
+//     `
+// }
+
+
+// for (let index = 1; index < 11; index++) {
+//      console.log(`1*${index}=${index}`) 
+//     }
+
+//     for (let index = 1; index < 11; index++) {
+//         const result = 5*index
+//         console.log(`5*${index}=${result}`) 
+//        }
+
+       for (let i = 0; i <= 10; i += 1) {
+        console.log(i)
+        if (i === 5) {
+            break;
+        }
+    }
