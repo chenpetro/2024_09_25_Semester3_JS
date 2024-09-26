a393fbff1d2c29efd49ff9e13764ce88779c043d
chenpetro <chenpetro@gmail.com>
2024-09-26 17:49:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let i = 0
+// while (i<100) {
+//     i++
+//     console.log(`this is my ${i} excercise`)
+//   }
+
+let i = 0;
+do {
+  i++;
+  console.log(`this is my ${i} excercise`);
+} while (i < 100);
+
+// let password = '';
+// do {
+//   password = prompt('Enter password longer than 4 symbols', '');
+// } while (password.length < 5);
+
+// console.log('Entered password: ', password);
